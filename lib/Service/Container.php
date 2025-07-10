83b24cbcde418c8124ae9fad246f33ee010f06e7
a.odegov <a.odegov@oos.ru>
2025-07-10 12:35:16 +0300

    release v1.0.2

--- lib/Service/Container.php
+++ lib/Service/Container.php
@@ -2,13 +2,11 @@
 
 namespace Base\Module\Service;
 
+use Base\Module\Exception\ModuleException;
 use Bitrix\Main\DI\ServiceLocator;
-use Bitrix\Main\ObjectNotFoundException;
-use Bitrix\Main\SystemException;
-use Psr\Container\NotFoundExceptionInterface;
-use ReflectionException;
 use Base\Module\Service\Tool\ClassList as IClassList;
 use ReflectionClass;
+use Throwable;
 
 class Container
 {
@@ -18,57 +16,61 @@ class Container
     /**
      * @param string $serviceCode
      * @return mixed
-     * @throws NotFoundExceptionInterface
-     * @throws ObjectNotFoundException
-     * @throws ReflectionException
-     * @throws SystemException
+     * @throws ModuleException
      */
     public static function get(string $serviceCode): mixed
     {
-        if (!isset(self::$serviceLocator)) {
-            self::$serviceLocator = ServiceLocator::getInstance();
+        try {
+            if (!isset(self::$serviceLocator)) {
+                self::$serviceLocator = ServiceLocator::getInstance();
+            }
+
+            if (!self::$lazyServicesLoaded) {
+                self::addLazyServices();
+                self::$lazyServicesLoaded = true;
+            }
+            $service = self::$serviceLocator->get($serviceCode);
+
+        } catch (Throwable $t) {
+            throw new ModuleException($t->getMessage(), $t->getCode());
         }
 
-        if (!self::$lazyServicesLoaded) {
-            self::addLazyServices();
-            self::$lazyServicesLoaded = true;
-        }
-
-        return self::$serviceLocator->get($serviceCode);
+        return $service;
     }
 
     /**
      * @return void
-     * @throws NotFoundExceptionInterface
-     * @throws ObjectNotFoundException
-     * @throws ReflectionException
-     * @throws SystemException
+     * @throws ModuleException
      */
     private static function addLazyServices(): void
     {
-        $classListService = self::$serviceLocator->get(IClassList::SERVICE_CODE);
-        if (!is_object($classListService) ||
-            !method_exists($classListService, 'getFromLib') ||
-            !method_exists($classListService, 'setSubClassesFilter')) {
-            return;
-        }
+        try {
+            $classListService = self::$serviceLocator->get(IClassList::SERVICE_CODE);
+            if (!is_object($classListService) ||
+                !method_exists($classListService, 'getFromLib') ||
+                !method_exists($classListService, 'setSubClassesFilter')) {
+                return;
+            }
 
-        $classes = $classListService->getFromLib("Src");
-        foreach ($classes as $className) {
-            $reflection = new ReflectionClass($className);
-            $attributes = $reflection->getAttributes(LazyService::class);
+            $classes = $classListService->getFromLib("Src");
+            foreach ($classes as $className) {
+                $reflection = new ReflectionClass($className);
+                $attributes = $reflection->getAttributes(LazyService::class);
 
-            if (!empty($attributes)) {
-                $lazyService = $attributes[0]->newInstance();
-                $serviceCode = $lazyService->serviceCode;
+                if (!empty($attributes)) {
+                    $lazyService = $attributes[0]->newInstance();
+                    $serviceCode = $lazyService->serviceCode;
 
-                if (!self::$serviceLocator->has($serviceCode)) {
-                    self::$serviceLocator->addInstanceLazy($serviceCode, [
-                        "className" => $className,
-                        "constructorParams" => $lazyService->constructorParams,
-                    ]);
+                    if (!self::$serviceLocator->has($serviceCode)) {
+                        self::$serviceLocator->addInstanceLazy($serviceCode, [
+                            "className" => $className,
+                            "constructorParams" => $lazyService->constructorParams,
+                        ]);
+                    }
                 }
             }
+        } catch (Throwable $t) {
+            throw new ModuleException($t->getMessage(), $t->getCode());
         }
     }
 }
